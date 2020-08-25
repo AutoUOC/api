11e55a72c0bdb768310549cdd771ef4b30cec278
BreMea <63671187+BreMea@users.noreply.github.com>
2020-08-24 20:07:16 -0400

    Added exports

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Posting Comments
+    comment = require('./methods/comment.js'),
+    
+    // Forum Activity
+    post = require('./methods/post.js'),
+    edit = require('./methods/edit.js')
+}
