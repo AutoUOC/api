89290c4c41b6abdd4f3cf415c32a3998b98e44a1
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-08-26 09:36:39 +0100

    Added getPostContent & getPostAuthor

--- methods/forums.js
+++ methods/forums.js
@@ -2,6 +2,7 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+const fetch = require("node-fetch");
 
 // Fetch authentithication stuff
 const cookieAuth = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../auth/cookies.json')));
@@ -130,5 +131,50 @@ module.exports = {
         // Send content and end request
         req.write(content);
         req.end();
-    }
+    },
+    async getPostContent(topicID, postNum) { 
+        const response = await fetch("https://scratch.mit.edu/discuss/topic/" + topicID + "/?page=" + (Math.floor(postNum / 10)+1))
+        if (response.status === 403) {
+                return {
+                    'code': response.status,
+                    'error-msg': 'Invalid auth',
+                    'data': 'none'
+                };
+            } else if (res.status === 500) {
+                return {
+                    'code': response.status,
+                    'error-msg': 'Server issues',
+                    'data': 'none'
+                };
+            } else {
+                const pageHTML = await response.text();
+                return {
+                    'code': response.status,
+                    'msg': 'Got post number ' + postNum + " from topic " + topicID,
+                    'data': pageHTML.split('<div class="post_body_html">')[(postNum%10)+1].split('</div>')[0]
+            };
+    },
+        async getPostAuthor(topicID, postNum) { 
+        const response = await fetch("https://scratch.mit.edu/discuss/topic/" + topicID + "/?page=" + (Math.floor(postNum / 10)+1))
+        if (response.status === 403) {
+                return {
+                    'code': response.status,
+                    'error-msg': 'Invalid auth',
+                    'data': 'none'
+                };
+            } else if (res.status === 500) {
+                return {
+                    'code': response.status,
+                    'error-msg': 'Server issues',
+                    'data': 'none'
+                };
+            } else {
+                const pageHTML = await response.text();
+                return {
+                    'code': response.status,
+                    'msg': 'Got author of post number ' + postNum + " from topic " + topicID,
+                    'data': pageHTML.split('<a class="black username" href="/users/')[(postNum % 10)+1].split('/"')[0]
+            }
+    },
+        
 }
